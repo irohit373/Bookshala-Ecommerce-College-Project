fd315cb34566b1636de8a3d39a3dfea005c97bc6
Rohit <deshmukhrohit373@gmail.com>
2023-04-19 16:05:42 +0530

    added tailwind CSS via CDN in index file

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+
+    <!-- CDN Tailwind CSS  -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Tailwind CSS CDN link -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <center>
+         <h1 class="text-green-500 text-4xl font-bold">
+        BookShala
+    </h1>
+    <strong>Buy Sell effortlessly</strong>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error delectus voluptates a aspernatur, asperiores magni molestiae eligendi dolorum eveniet odio fuga ea vero quaerat numquam!.</p>
+    </center>
+</body>
+</html>
