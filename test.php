3998f67144cb624027bdddcf7b4a8a5d10094784
Rohit <deshmukhrohit373@gmail.com>
2023-04-19 11:41:28 +0530

    added intinal files and folders to work on...

---
+++ test.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>check 1</title>
+</head>
+<body>
+	<center><p>Checking the XAMPP git and connection...</p>
+	</br></br>
+	<h2>!Done Good to Go!</h2><center>
+</body>
+</html>
