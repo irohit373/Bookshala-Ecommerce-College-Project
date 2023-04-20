7e65d6309bfc2f1574f7964bef3e338028bdfd1a
Rohit <deshmukhrohit373@gmail.com>
2023-04-20 14:48:00 +0530

    commented tailwind and implemented CDN Bootstrap

--- index.php
+++ index.php
@@ -7,18 +7,26 @@
     <title>Index Page</title>
 
     <!-- CDN Tailwind CSS  -->
-    <script src="https://cdn.tailwindcss.com"></script>
-
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
     <!-- Tailwind CSS CDN link -->
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> -->
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+
 </head>
 <body>
     <center>
-         <h1 class="text-green-500 text-4xl font-bold">
-        BookShala
-    </h1>
+         <h1 class="text-green-500 text-4xl font-bold">    BookShala    </h1>
     <strong>Buy Sell effortlessly</strong>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error delectus voluptates a aspernatur, asperiores magni molestiae eligendi dolorum eveniet odio fuga ea vero quaerat numquam!.</p>
     </center>
+
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </body>
 </html>
