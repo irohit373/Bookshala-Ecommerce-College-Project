e76230f566579c3e6f3cf0495e466726a64299d7
Rohit Deshmukh <deshmukhrohit373@gmail.com>
2023-04-24 15:42:54 +0530

    comments for don't touch someitems

--- register_page.php
+++ register_page.php
@@ -8,33 +8,43 @@
 </head>
 <body>
    <center> 
-    <form action="function/register.php" method="post" class="form">
+      <!-- Do not touch this -->
+      <form action="function/register.php" method="post" class="form">
+      <!-- this -->
         <br>
         <h1>Sign Up</h1>
         <br>
 
         <div><span></span>
+        
+        <!-- Do not touch this -->            
         <input type="text" name="fname" placeholder="First Name" class="form-control" autofocus="">
         <input type="text" name="lname" placeholder="Last Name" class="form-control">
+        <!-- this -->
         </div>
 
         <br>
         
         <div>
-            <input type="email" name="email" class="form-control" placeholder="example@email.com">
+                        <!-- Do not touch this -->                        
+                        <input type="email" name="email" class="form-control" placeholder="example@email.com">
+                        <!-- this -->
         </div>
 
         <br>
 
         <div>
             <span></span>
+            <!-- Do not touch this -->
             <input type="password" name="password" class="form-control" placeholder="Password">
             <input type="password" name="password2" class="form-control" placeholder="Confirm Password">
+            <!-- this -->
         </div>
 
         <br>
-
-        <button type="submit" name="submit"> Register </button>
+            <!-- Do not touch this -->            
+            <button type="submit" name="submit"> Register </button>
+            <!-- this -->
     </form>
     </center>
 </body>
